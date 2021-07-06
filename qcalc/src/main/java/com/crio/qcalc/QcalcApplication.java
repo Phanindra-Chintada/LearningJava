83797cee3cc97cd1e7d2d5685a3daf7abc19d8e0
Phanindra-Chintada <phanindra.chintada@gmail.com>
2021-07-06 10:59:00 +0530

    Creating project

---
+++ qcalc/src/main/java/com/crio/qcalc/QcalcApplication.java
@@ -0,0 +1,16 @@
+package com.crio.qcalc;
+
+// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcalcApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(QcalcApplication.class, args);
+		System.out.print("Hello World");
+		System.out.print(5 + 10 + "Hello World ");
+		System.out.print("Hello World" + 5 + 10);
+
+	}
+}
